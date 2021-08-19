aab9b69adfbc8f141ddfa7013567bdacf1bf2831
Martin Bruse <zondolfin@gmail.com>
2021-08-19 12:15:41 +0200

    Changed the beta app to point to the new beta domain name.

--- app/src/beta/java/se/oort/diplicity/Config.kt
+++ app/src/beta/java/se/oort/diplicity/Config.kt
@@ -1,4 +1,4 @@
 package se.oort.diplicity
 
-const val CONFIG_SERVER_URL = "https://beta-dot-dipact.appspot.com"
+const val CONFIG_SERVER_URL = "https://beta.diplicity.com"
 const val CONFIG_API = "beta"
